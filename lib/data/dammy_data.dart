da8eb24079149987a815d89fb32053d3f8b693b4
Rits Jheckson Sa <jheckson@pop-os.localdomain>
2022-06-28 14:25:55 -0300

    add new data

--- lib/data/dammy_data.dart
+++ lib/data/dammy_data.dart
@@ -3,19 +3,27 @@ import 'chat.dart';
 class DammyData {
   static const List<Chat> ChatList = [
     Chat(
-        contactName: '61991801234',
-        avatar: '',
-        dateMsg: '30/03/2022',
-        isPin: false,
-        description: 'lorem ipsum',
-        coutMsg: 3),
-    Chat(
         contactName: 'Michelangelo',
         avatar:
             'https://veja.abril.com.br/wp-content/uploads/2021/09/Michelangelo_Daniele_da_Volterra_dettaglio.jpg',
         dateMsg: '19/03/1553',
         isPin: true,
         description: 'foi um pintor, escultor, poeta...',
-        coutMsg: 0),
+        coutMsg: 30),
+    Chat(
+        contactName: 'Nikola Tesla',
+        avatar:
+            'https://www.greenmebrasil.com/wp-content/uploads/2017/06/nikola_tesla-1200x900.jpg',
+        dateMsg: '15/09/1883',
+        isPin: true,
+        description: 'engenheiro eletrotécnico...',
+        coutMsg: 2),
+    Chat(
+        contactName: '00 0 00000000',
+        avatar: '',
+        dateMsg: '30/03/2022',
+        isPin: false,
+        description: 'lorem ipsum',
+        coutMsg: 3),
   ];
 }
